b654dee8e0ffffa010660e71b61455b44bcce3ae
Nick Zhang <thesadboy@qq.com>
2013-06-03 18:34:11 +0800

    +server.js for openshift

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express')
+	, routes = require('./routes/routes.js')
+	, http = require('http')
+	, path = require('path')
+	, fs = require("fs")
+	,MongoStore = require("connect-mongo")(express)
+	, argv = require("optimist").default("config",process.cwd()+"/config.json").argv
+	, cache = require("memory-cache");
+
+var app = express();
+try{
+	options = JSON.parse(fs.readFileSync(argv.config));
+} catch(e)
+{
+	throw new Error("could not read config " + argv.config + "internal error: " + e.toString());
+}
+// all environments
+app.set('port', process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT ||  options.port || 3000);
+app.set('ipaddr',process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('options',options);
+app.set('cache',cache);
+app.use(express.favicon(__dirname + '/public/favicon.ico'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+	secret : options.database.cookieSecret,
+	cookies:{ maxAge: 24* 60 * 60 * 1000 }
+	// cookies:{ maxAge: 24* 60 * 60 * 1000 },
+	// store : new MongoStore({
+	// 		url : options.database.url
+	// 	})
+	})
+);
+app.use(function(req,res,next){
+	res.locals.user = req.session.user || null;
+	next();
+});
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+routes(app);
+http.createServer(app).listen(app.get('ipaddr'),app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
